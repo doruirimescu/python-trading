3324f52705f1dd84789271973d8be36f422dbdfb
Doru Irimescu <doru.irimescu@sensible4.fi>
2021-01-30 20:22:11 +0200

    Update installation for xtb api

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,2 +1,10 @@
+# For investing api
 pip install bs4
 pip install lxml
+
+# For xtb api
+git clone git@github.com:federico123579/XTBApi.git
+cd XTBApi/
+python3 -m venv env
+. env/bin/activate
+pip install .
