b58e0b4232b9cec6069cab8e090fda251dc86d35
Doru Irimescu <doru.irimescu@sensible4.fi>
2021-01-30 21:05:30 +0200

    Use forked version

--- install_requirements.sh
+++ install_requirements.sh
@@ -3,7 +3,7 @@ pip install bs4
 pip install lxml
 
 # For xtb api
-git clone git@github.com:federico123579/XTBApi.git
+git clone git@github.com:doruirimescu/XTBApi.git
 cd XTBApi/
 python3 -m venv env
 . env/bin/activate
