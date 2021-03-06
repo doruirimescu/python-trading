24dc25536ce465573cfb07b2dd5c1d1d94868771
Doru Irimescu <doru.irimescu@sensible4.fi>
2021-03-06 23:04:58 +0200

    Split functionality into files

--- InvestingAPI/investing_candlestick.py
+++ InvestingAPI/investing_candlestick.py
@@ -4,20 +4,11 @@ from enum import Enum
 from datetime import datetime
 import symbols_url
 
-# Defines a response from investing.com
-class InvestingAnalysisResponse(Enum):
-    """Enumeration class for investing.com analysis response"""
-
-    STRONG_SELL = "Strong Sell"
-    SELL = "Sell"
-    NEUTRAL = "Neutral"
-    BUY = "Buy"
-    STRONG_BUY = "Strong Buy"
-
 class InvestingCandlestickAnalysisReliability(Enum):
     LOW = 'Low'
     MEDIUM = 'Medium'
     HIGH = 'High'
+
 class InvestingCandleStickAnalysisResponse:
     def __init__(self, pattern, timeframe, reliability, candles_ago, date):
         self.pattern = pattern
@@ -25,8 +16,8 @@ class InvestingCandleStickAnalysisResponse:
         self.reliability = reliability
         self.candles_ago = candles_ago
         self.date = date
-##TODO split into InvestingTechnical and InvestingCandlestick classes in different files
-class Investing:
+
+class InvestingCandlestickAnalysis:
     def __init__(self):
 
         # symbols maps a symbol to a tuple (address, pairID) - find the pairID by inspecting the network traffic response
@@ -36,46 +27,7 @@ class Investing:
         response = [i for i, j in self.symbols.items()]
         return response
 
-    def getTechnicalAnalysis(self, symbol, period):
-        symbol = symbol.upper()
-        headers = {
-            "User-Agent": "Mozilla/5.0",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Referer": self.symbols[symbol][0],
-            "X-Requested-With": "XMLHttpRequest",
-        }
-
-        periods = {
-            "1m": 60,
-            "5m": 300,
-            "15m": 900,
-            "30m": 1800,
-            "1h": 3600,
-            "5h": 18000,
-            "D": 86400,
-            "W": "week",
-            "M": "month",
-        }
-
-        body = {"pairID": self.symbols[symbol][1], "period": "", "viewType": "normal"}
-
-        with requests.Session() as s:
-            body["period"] = periods[period]
-            r = s.post(
-                "https://www.investing.com/instruments/Service/GetTechincalData",
-                data=body,
-                headers=headers,
-            )
-            soup = bs(r.content, "lxml")
-            response = list()
-
-            for i in soup.select("#techStudiesInnerWrap .summary"):
-                response_text = i.select("span")[0].text
-                print(response_text)
-                response.append(InvestingAnalysisResponse(response_text))
-        return response
-
-    def sanitizeTimeframe(self, timeframe):
+    def __sanitizeTimeframe(self, timeframe):
         if timeframe == "1":
             return "1m"
         elif timeframe == "5":
@@ -86,7 +38,7 @@ class Investing:
             return "30m"
         return timeframe
 
-    def getTimeFormatter(self, timeframe):
+    def __getTimeFormatter(self, timeframe):
         if timeframe =='1m':
             return "%b %d, %Y %I:%M%p"
         elif timeframe =='5m':
@@ -106,7 +58,7 @@ class Investing:
         elif timeframe =='1M':
             return "%b %y"
 
-    def getCandlestickAnalysis(self, symbol, period=None):
+    def getAnalysis(self, symbol, period=None):
         ret = list()
 
         symbol = symbol.upper()
@@ -138,7 +90,7 @@ class Investing:
                         pattern = child.contents[0]
                     elif counter == 5:
                         timeframe = child.contents[0]
-                        timeframe = self.sanitizeTimeframe(timeframe)
+                        timeframe = self.__sanitizeTimeframe(timeframe)
                     elif counter == 7:
                         reliability = InvestingCandlestickAnalysisReliability(child["title"])
                     elif counter == 9:
@@ -151,7 +103,7 @@ class Investing:
 
                     #Check that it is not a current candle
                     if(date is not ''):
-                        date = datetime.strptime(date, self.getTimeFormatter(timeframe))
+                        date = datetime.strptime(date, self.__getTimeFormatter(timeframe))
 
                     print("Pattern: \t" + pattern)
                     print("Timeframe: \t" + timeframe)
@@ -164,8 +116,5 @@ class Investing:
                 row_id += 1
                 table = soup.find("tr", id="row" + str(row_id))
 
-i = Investing()
-
-print(i.getTechnicalAnalysis("BTCUSD", "30m"))
-
-#i.getCandlestickAnalysis("BTCUSD")
+i = InvestingCandlestickAnalysis()
+i.getAnalysis("BTCUSD")
---
+++ InvestingAPI/investing_technical.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from enum import Enum
+from datetime import datetime
+import symbols_url
+
+# Defines a response from investing.com
+class InvestingAnalysisResponse(Enum):
+    """Enumeration class for investing.com analysis response"""
+
+    STRONG_SELL = "Strong Sell"
+    SELL = "Sell"
+    NEUTRAL = "Neutral"
+    BUY = "Buy"
+    STRONG_BUY = "Strong Buy"
+
+class Investing:
+    def __init__(self):
+
+        # symbols maps a symbol to a tuple (address, pairID) - find the pairID by inspecting the network traffic response
+        self.symbols = symbols_url.SYMBOLS_URL
+
+    def getAvailableSymbols(self):
+        response = [i for i, j in self.symbols.items()]
+        return response
+
+    def getAnalysis(self, symbol, period):
+        symbol = symbol.upper()
+        headers = {
+            "User-Agent": "Mozilla/5.0",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Referer": self.symbols[symbol][0],
+            "X-Requested-With": "XMLHttpRequest",
+        }
+
+        periods = {
+            "1m": 60,
+            "5m": 300,
+            "15m": 900,
+            "30m": 1800,
+            "1h": 3600,
+            "5h": 18000,
+            "D": 86400,
+            "W": "week",
+            "M": "month",
+        }
+
+        body = {"pairID": self.symbols[symbol][1], "period": "", "viewType": "normal"}
+
+        with requests.Session() as s:
+            body["period"] = periods[period]
+            r = s.post(
+                "https://www.investing.com/instruments/Service/GetTechincalData",
+                data=body,
+                headers=headers,
+            )
+            soup = bs(r.content, "lxml")
+            response = list()
+
+            for i in soup.select("#techStudiesInnerWrap .summary"):
+                response_text = i.select("span")[0].text
+                print(response_text)
+                response.append(InvestingAnalysisResponse(response_text))
+        return response
+
+
+i = Investing()
+print(i.getAnalysis("BTCUSD", "30m"))
