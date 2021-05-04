84e7040037325071822da2741c4d2bd7c437b366
Doru Irimescu <doru.irimescu@sensible4.fi>
2021-05-04 21:38:01 +0300

    Add trader code

--- Trading/Live/Trader/trader.py
+++ Trading/Live/Trader/trader.py
@@ -1,4 +1,25 @@
-from Trading.Live.S
+from Trading.Algo.Strategy.strategy import *
+from Trading.Live.ExceptionWithRetry.exceptionwithretry import ExceptionWithRetry
+from Trading.Live.Client.client import XTBLoggingClient, Client
+from Trading.Instrument.instrument import Instrument
+from Trading.Live.InvestingAPI.investing_technical import *
 
 class Trader:
-    pass
+    def __init__(self, instrument: Instrument, client: Client, technical_analyzer):
+
+        self._instrument = instrument
+        self._client = client
+        self._technical_analyzer = technical_analyzer
+
+    def _getTechnicalAnalysis(self):
+        ewr = ExceptionWithRetry(self._technical_analyzer.analyse, 10, 1.0)
+        analysis = ewr.run([self._symbolToInvesting(), self._instrument.timeframe])
+        return analysis
+
+    def _symbolToInvesting(self):
+        if self._instrument.symbol == "BITCOIN":
+            return "BTCUSD"
+        elif self._instrument.symbol == "ETHEREUM":
+            return "ETHUSD"
+        else:
+            return self._instrument.symbol
