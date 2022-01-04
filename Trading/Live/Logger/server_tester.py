bb74c347191ca7644ddbf4efe0ef11cdcb07f59d
Doru Irimescu <doru.irimescu@sensible4.fi>
2022-01-04 19:41:44 +0200

    Add comments

--- Trading/Live/Logger/server_tester.py
+++ Trading/Live/Logger/server_tester.py
@@ -1,26 +1,38 @@
 from datetime import datetime
 from dataclasses import dataclass
 import logging
-#TODO: Use ExceptionWithRetry
+# TODO: Use ExceptionWithRetry
 __all = ['ServerTest', 'ServerTester']
+
 
 @dataclass
 class ServerTest:
+    """Server Test result object."""
     is_server_up: bool
     error: str
+
 
 # create logger
 logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO)
 logger = logging.getLogger('Server tester')
 logger.setLevel(logging.INFO)
 
+
 class ServerTester:
-    logger:logging
+    """Tests the connection to a trading server.
+    """
+    logger: logging
+
     def __init__(self, client):
         logger.info("Initialized Server Tester")
         self._client = client
 
     def test(self):
+        """Tries to login to the server. Then, it will ping the server.
+
+        Returns:
+            ServerTest: Result of the server test
+        """
         try:
             logger.info("Try login")
             self._client.login()
