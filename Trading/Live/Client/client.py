810c9d5a3e4dd329cd8b14ccd346bf277296a003
Doru Irimescu <doru.irimescu@sensible4.fi>
2022-06-07 20:10:05 +0300

    Add format and fix XTBTradingClient's closeTrade

--- Trading/Live/Client/client.py
+++ Trading/Live/Client/client.py
@@ -7,6 +7,8 @@ from Trading.Instrument.timeframes import TIMEFRAME_TO_MINUTES
 
 from XTBApi.api import Client as XTBClient
 from Trading.Live.Logger.server_tester import *
+
+
 class LoggingClient(ABC):
     @abstractmethod
     def __init__(self, uname=0, pwd=0, mode=0):
@@ -15,6 +17,7 @@ class LoggingClient(ABC):
     @abstractmethod
     def getLastNCandleHistory(self):
         pass
+
 
 class TradingClient(ABC):
     @abstractmethod
@@ -33,8 +36,9 @@ class TradingClient(ABC):
     def closeTrade(self, trade_id):
         pass
 
+
 class XTBTradingClient():
-    def __init__(self, uname, pwd, mode="demo", logging = False):
+    def __init__(self, uname, pwd, mode="demo", logging=False):
         self._client = XTBClient(uname, pwd, mode, logging)
         self._server_tester = ServerTester(self._client)
 
@@ -58,13 +62,12 @@ class XTBTradingClient():
         """Closes a trade by trade id
         """
         self._client.login()
-        response = self._client.close_trade_fix(trade_id)
+        response = self._client.close_trade_fix(trade_id['order'])
         self._client.logout()
 
 
-#! Use only centralized market instruments, otherwise there will be a large variation in
 class XTBLoggingClient(LoggingClient):
-    def __init__(self, uname, pwd, mode="demo", logging = False):
+    def __init__(self, uname, pwd, mode="demo", logging=False):
         self._client = XTBClient(uname, pwd, mode, logging)
         self._server_tester = ServerTester(self._client)
 
@@ -74,14 +77,15 @@ class XTBLoggingClient(LoggingClient):
 
         instrument.symbol = instrument.symbol
         self._client.login()
-        hist = self._client.get_lastn_candle_history(instrument.symbol, TIMEFRAME_TO_MINUTES[instrument.timeframe] * 60, N)
+        hist = self._client.get_lastn_candle_history(
+            instrument.symbol, TIMEFRAME_TO_MINUTES[instrument.timeframe] * 60, N)
         self._client.logout()
 
-        open    = list()
-        high    = list()
-        low     = list()
-        close   = list()
-        date    = list()
+        open = list()
+        high = list()
+        low = list()
+        close = list()
+        date = list()
 
         for ohlct in hist:
             open.append(ohlct['open'])
@@ -89,7 +93,7 @@ class XTBLoggingClient(LoggingClient):
             low.append(ohlct['low'])
             close.append(ohlct['close'])
             date.append(datetime.fromtimestamp(ohlct['timestamp']))
-        return {'open':open, 'high':high, 'low':low, 'close':close, 'date': date}
+        return {'open': open, 'high': high, 'low': low, 'close': close, 'date': date}
 
     def _isServerUp(self):
         test = self._server_tester.test()
