011034d071d7855d3edd91965298431c372f2529
Doru Irimescu <doru.irimescu@sensible4.fi>
2022-06-07 21:59:10 +0300

    Fix trader

--- Trading/Live/Trader/trader.py
+++ Trading/Live/Trader/trader.py
@@ -7,6 +7,13 @@ import logging
 
 
 class Trader:
+    '''
+        Trader uses a client and a technical analyzer in order to decide what to do.
+        It processes one trade at a time.
+        It places a trade according to the decision.
+        It also stops the current trade.
+    '''
+
     def __init__(self, instrument: Instrument, client: TradingClient, technical_analyzer):
 
         self._instrument = instrument
@@ -15,6 +22,7 @@ class Trader:
         self.previous_analysis = None
         self.current_trade = None
         self.LOGGER = logging.getLogger('Trader')
+        self.LOGGER.info("Create trader")
 
     def _getTechnicalAnalysis(self):
         ewr = ExceptionWithRetry(self._technical_analyzer.analyse, 10, 1.0)
@@ -28,16 +36,18 @@ class Trader:
             return
         current_analysis = self._getTechnicalAnalysis()
         action = decideAction(self.previous_analysis, current_analysis)
-
+        self.previous_analysis = current_analysis
         print(action)
+
         if action == Action.NO:
             self.LOGGER.info("No action shall be taken")
-        elif action == Action.BUY:
+        elif action == Action.BUY and self.current_trade is None:
             self.LOGGER.info("Client will buy. Should decide volume according to optimal risk")
             self.current_trade = self._client.buy(self._instrument, 1.0)
-        elif action == Action.SELL:
+        elif action == Action.SELL and self.current_trade is None:
             self.LOGGER.info("Client will sell. Should decide volume according to optimal risk")
             self.current_trade = self._client.sell(self._instrument, 1.0)
         elif action == Action.STOP and self.current_trade is not None:
             self.LOGGER.info("Client will stop the current trade")
             self._client.closeTrade(self.current_trade)
+            self.current_trade = None
