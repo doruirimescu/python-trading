8dbf10ee26d144186f1c8a6f4f28009c3724e4c7
Doru Irimescu <doru.irimescu@sensible4.fi>
2022-06-07 21:43:19 +0300

    Fix requirements

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,7 +1,4 @@
-# For investing api
-pip install bs4
-pip install lxml
-pip install argparse
+pip3 install -r requirements.txt
 
 # For xtb api
 git clone git@github.com:doruirimescu/XTBApi.git
@@ -9,16 +6,3 @@ cd XTBApi/
 python3 -m venv env
 . env/bin/activate
 pip install .
-
-# For candle
-pip install pandas
-pip install plotly
-
-#For tests
-pip3 install pytest
-
-#Install aenum
-pip install aenum
-
-pip install requests
-pip install python-dotenv
