dbc192fedff3ae1aab68f86b102b392731826dec
Doru Irimescu <doru.irimescu@sensible4.fi>
2022-06-07 19:40:54 +0300

    Remove yfinance timeframes

--- Trading/Instrument/timeframes.py
+++ Trading/Instrument/timeframes.py
@@ -1,5 +1,3 @@
-__all__=['TIMEFRAMES', 'TIMEFRAME_TO_MINUTES']
+__all__ = ['TIMEFRAMES', 'TIMEFRAME_TO_MINUTES']
 TIMEFRAMES = ['1m', '5m', '15m', '30m', '1h', '5h', '1D', '1W', '1M']
-TIMEFRAME_TO_MINUTES=dict(zip(TIMEFRAMES, [1, 5, 15, 30, 60, 300, 1440, 10080, 43200]))
-
-TIMEFRAME_TO_YFINANCE={'1m': '1m', '5m': '5m', '15m': '15m', '30m': '30m', '1h': '1h', '1D': '1d', '1W': '1w', '1M':'1mo'}
+TIMEFRAME_TO_MINUTES = dict(zip(TIMEFRAMES, [1, 5, 15, 30, 60, 300, 1440, 10080, 43200]))
