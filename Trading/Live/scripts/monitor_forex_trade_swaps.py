6432156e5119bf3e6bf404e33506a8006c6df836
Doru Irimescu <>
2023-01-25 13:07:43 +0200

    Add monitor_forex scripts

---
+++ Trading/Live/scripts/monitor_forex_trade_swaps.py
@@ -0,0 +1,49 @@
+from Trading.Live.Client.client import XTBTradingClient
+from Trading.Live.Email.send_email import send_email
+
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+from Trading.Live.Logger.ticker import Ticker
+import logging
+from time import sleep
+
+
+if __name__ == '__main__':
+
+    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=FORMAT)
+
+    MAIN_LOGGER = logging.getLogger('Main Logger')
+    MAIN_LOGGER.setLevel(logging.DEBUG)
+    MAIN_LOGGER.propagate = True
+
+    load_dotenv()
+    username = os.getenv("XTB_USERNAME")
+    password = os.getenv("XTB_PASSWORD")
+    mode = os.getenv("XTB_MODE")
+    client = XTBTradingClient(username, password, mode, False)
+
+    open_trade_swaps = client.getSwapsOfForexOpenTrades()
+    MAIN_LOGGER.info("Open trade swaps: " + str(open_trade_swaps))
+
+    while True:
+        hour_now = datetime.now().hour
+        minute_now = datetime.now(). minute
+
+        if hour_now == 7:
+            # Each morning at 7 am
+            for symbol, swap in open_trade_swaps:
+                if swap < 0.0:
+                    subject = "Swap has gone negative for " + symbol
+                    body = f"Symbol: {symbol} Swap: {str(swap)} Date:{str(datetime.now())}"
+                    recipients = ["dorustefan.irimescu@gmail.com"]
+                    send_email(subject, body, recipients)
+
+            total_profit, total_swap, text_message = client.getTotalForexOpenTradesProfitAndSwap()
+            subject = "Daily swap report" + str(datetime.now())
+            body = text_message + f"\nTotal profit: {str(total_profit)} Total swap: {str(total_swap)}"
+            recipients = ["dorustefan.irimescu@gmail.com"]
+            send_email(subject, body, recipients)
+            MAIN_LOGGER.info(body)
+            sleep(3600)
--- Trading/Live/scripts/monitor_forex_trades.py
+++ Trading/Live/scripts/monitor_forex_trades.py
@@ -1,9 +1,8 @@
 from Trading.Live.Client.client import XTBTradingClient
-from Trading.Live.Trader.trader import Trader
-from Trading.Instrument.instrument import Instrument
-from Trading.Live.InvestingAPI.investing_technical import TechnicalAnalyzer
+from Trading.Live.Email.send_email import send_email
+
 from dotenv import load_dotenv
-import time
+from datetime import datetime
 import os
 from Trading.Live.Logger.ticker import Ticker
 import logging
@@ -24,6 +23,23 @@ if __name__ == '__main__':
     mode = os.getenv("XTB_MODE")
     client = XTBTradingClient(username, password, mode, False)
 
-    total_profit, total_swap = client.getTotalForexOpenTradesProfitAndSwap()
-    print("Total profit", total_profit)
-    print("Total swap", total_swap)
+    # Get profit and swap of current trades
+    # total_profit, total_swap = client.getTotalForexOpenTradesProfitAndSwap()
+    # print("Total profit", total_profit)
+    # print("Total swap", total_swap)
+
+    # biggest_swaps = client.getTopTenBiggestSwaps()
+    # subject = "Top ten biggest swaps as of " + str(datetime.now())
+    # body = str(biggest_swaps)
+    # recipients = ["dorustefan.irimescu@gmail.com"]
+    # send_email(subject, body, recipients)
+
+    # open_trade_swaps = client.getSwapsOfForexOpenTrades()
+    # print(open_trade_swaps)
+
+    # for symbol, swap in open_trade_swaps:
+    #     if swap < 0.0:
+    #         subject = "Swap has gone negative for " + symbol
+    #         body = f"Symbol: {symbol} Swap: {str(swap)} Date:{str(datetime.now())}"
+    #         recipients = ["dorustefan.irimescu@gmail.com"]
+    #         send_email(subject, body, recipients)
