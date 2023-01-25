d04d1f390f0c143e1b1cf770f8c3700aa4767647
Doru Irimescu <>
2023-01-25 19:45:50 +0200

    Make script run only once for crontab

--- Trading/Live/scripts/monitor_forex_trade_swaps.py
+++ Trading/Live/scripts/monitor_forex_trade_swaps.py
@@ -21,28 +21,34 @@ if __name__ == '__main__':
     username = os.getenv("XTB_USERNAME")
     password = os.getenv("XTB_PASSWORD")
     mode = os.getenv("XTB_MODE")
+    monitor_forex_trade_swaps_once = bool(os.getenv("MONITOR_FOREX_TRADE_SWAPS_ONCE"))
     client = XTBTradingClient(username, password, mode, False)
 
     open_trade_swaps = client.getSwapsOfForexOpenTrades()
     MAIN_LOGGER.info("Open trade swaps: " + str(open_trade_swaps))
 
-    while True:
-        hour_now = datetime.now().hour
-        minute_now = datetime.now(). minute
+    def monitor_once():
+        for symbol, swap in open_trade_swaps:
+            if swap < 0.0:
+                subject = "Swap has gone negative for " + symbol
+                body = f"Symbol: {symbol} Swap: {str(swap)} Date:{str(datetime.now())}"
+                recipients = ["dorustefan.irimescu@gmail.com"]
+                send_email(subject, body, recipients)
 
-        if hour_now == 7:
-            # Each morning at 7 am
-            for symbol, swap in open_trade_swaps:
-                if swap < 0.0:
-                    subject = "Swap has gone negative for " + symbol
-                    body = f"Symbol: {symbol} Swap: {str(swap)} Date:{str(datetime.now())}"
-                    recipients = ["dorustefan.irimescu@gmail.com"]
-                    send_email(subject, body, recipients)
+        total_profit, total_swap, text_message = client.getTotalForexOpenTradesProfitAndSwap()
+        subject = "Daily swap report" + str(datetime.now())
+        body = text_message + f"\nTotal profit: {str(total_profit)} Total swap: {str(total_swap)}"
+        recipients = ["dorustefan.irimescu@gmail.com"]
+        send_email(subject, body, recipients)
+        MAIN_LOGGER.info(body)
 
-            total_profit, total_swap, text_message = client.getTotalForexOpenTradesProfitAndSwap()
-            subject = "Daily swap report" + str(datetime.now())
-            body = text_message + f"\nTotal profit: {str(total_profit)} Total swap: {str(total_swap)}"
-            recipients = ["dorustefan.irimescu@gmail.com"]
-            send_email(subject, body, recipients)
-            MAIN_LOGGER.info(body)
-            sleep(3600)
+    if monitor_forex_trade_swaps_once:
+        monitor_once()
+    else:
+        while True:
+            hour_now = datetime.now().hour
+            minute_now = datetime.now(). minute
+            monitor_once()
+            if hour_now == 7:
+                # Each morning at 7 am
+                sleep(3600)
