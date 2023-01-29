023f2f9fdb1bdcef0f0bd44d24dd0cbe297d383e
Doru Irimescu <>
2023-01-29 13:26:14 +0200

    Improve exceptionwithretry

--- Trading/Live/ExceptionWithRetry/exceptionwithretry.py
+++ Trading/Live/ExceptionWithRetry/exceptionwithretry.py
@@ -1,6 +1,11 @@
 import time
+from typing import Callable, Any
+import functools
 
 class ExceptionWithRetry:
+    """Retries calling the function for n_retry times, with time_sleep in between unsuccessful attempts.
+       Does not throw any error if the last retry fails.
+    """
     def __init__(self, method, n_retry = 5, time_sleep = 0.5):
         self._method = method
         self._retries = 0
@@ -16,3 +21,34 @@ class ExceptionWithRetry:
                 print(e)
                 time.sleep(self._time_sleep)
                 return self.run(args)
+
+
+def exception_with_retry(n_retry: int,  sleep_time_s: float):
+    """Use this decorator to retry calling your function n_retry times, with sleep_time_s in between unsuccessful calls.
+    Will finally throw the original error if the last retry fails.
+
+    Args:
+        n_retry (int): number of retries
+        sleep_time_s (float): time to sleep between unsuccessful retries, in seconds
+
+    Returns:
+        Callable[..., Any]: decorated function
+    """
+    retries = 0
+    def dec(func: Callable[..., Any]) -> Callable[..., Any]:
+        @functools.wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            nonlocal retries
+            try:
+                retries += 1
+                return func(*args, **kwargs)
+            except Exception as e:
+                if retries < n_retry:
+                    print(e)
+                    time.sleep(sleep_time_s)
+                    return wrapper(*args, **kwargs)
+                else:
+                    raise e
+
+        return wrapper
+    return dec
--- Trading/Live/ExceptionWithRetry/tests/test_1.py
+++ Trading/Live/ExceptionWithRetry/tests/test_1.py
@@ -1,7 +1,16 @@
 import pytest
 import unittest
+from typing import List
 
-from Trading.Live.ExceptionWithRetry.exceptionwithretry import ExceptionWithRetry
+from Trading.Live.ExceptionWithRetry.exceptionwithretry import ExceptionWithRetry, exception_with_retry
+
+@exception_with_retry(3, 0.5)
+def wrapped_method(number_1: int, number_2: int = 0, calls: List = []):
+    calls.append(1)
+    if number_1 < 0:
+        raise Exception("Not gonna happen")
+    else:
+        return number_1 + number_2
 
 class TestMethods:
     def method(self, arg):
@@ -58,3 +67,30 @@ class TestExceptionWithRetry(unittest.TestCase):
         ewr = ExceptionWithRetry(method3, 5, 0.0)
         result = ewr.run([])
         self.assertEqual(result, 25)
+
+
+class TestExceptionWithRetryDecorator(unittest.TestCase):
+    def test_no_exception(self):
+        result = wrapped_method(number_1=1)
+        self.assertEqual(1, result)
+
+        calls = []
+        result = wrapped_method(number_1=1, number_2=10, calls=calls)
+        self.assertEqual(11, result)
+        self.assertEqual(1, len(calls))
+
+    def test_throws_exception(self):
+        with self.assertRaises(Exception):
+            calls = []
+            result = wrapped_method(-1, 0, calls)
+            self.assertEqual(3, len(calls))
+
+        with self.assertRaises(Exception):
+            calls = []
+            wrapped_method(number_1=-1, number_2=2)
+            self.assertEqual(3, len(calls))
+
+        with self.assertRaises(Exception):
+            calls = []
+            wrapped_method(-1, 2)
+            self.assertEqual(3, len(calls))
