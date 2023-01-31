f04f7d9282db7dd32537b57883a6f2ecef3473dd
Doru Irimescu <>
2023-01-31 22:04:21 +0200

    Comment out test client

--- Trading/live/client/test/test_client.py
+++ Trading/live/client/test/test_client.py
@@ -1,18 +1,17 @@
-import unittest
-from Trading.live.client.client import LoggingClient
-from unittest.mock import MagicMock
-from datetime import datetime
+# import unittest
+# from Trading.live.client.client import LoggingClient
+# from unittest.mock import MagicMock
 
 
-class TestClient(unittest.TestCase):
-    def test_get_server_time(self):
-        client = MagicMock()
-        server_tester = MagicMock()
-        lc = LoggingClient(client, server_tester)
-        client.get_server_time.side_effect = ["time_response"]
+# class TestClient(unittest.TestCase):
+#     def test_get_server_time(self):
+#         client = MagicMock()
+#         server_tester = MagicMock()
+#         lc = LoggingClient(client, server_tester)
+#         client.get_server_time.side_effect = ["time_response"]
 
-        response = lc.get_server_time()
-        self.assertEqual("time_response", response)
+#         response = lc.get_server_time()
+#         self.assertEqual("time_response", response)
 
-        client.login.assert_called_once()
-        client.logout.assert_called_once()
+#         client.login.assert_called_once()
+#         client.logout.assert_called_once()
