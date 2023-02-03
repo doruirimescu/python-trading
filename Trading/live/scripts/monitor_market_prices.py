06d6483259a74c9e90d6664ec798c8e01a67bcd6
Doru Irimescu <>
2023-02-03 13:19:41 +0200

    Add monitor market prices

---
+++ Trading/live/scripts/monitor_market_prices.py
@@ -0,0 +1,86 @@
+from exception_with_retry import exception_with_retry
+
+from Trading.live.client.client import XTBTradingClient
+from Trading.utils.send_email import send_email
+from Trading.config.config import USERNAME, PASSWORD, MODE, MONITOR_FOREX_TRADE_SWAPS_ONCE
+from Trading.live.alert.alert import (get_total_swap_of_open_forex_trades_report,
+                                     is_symbol_price_below_value,
+                                     is_symbol_price_above_value,
+                                     is_symbol_price_below_last_n_intervals_low,
+                                     is_symbol_price_above_last_n_intervals_low)
+from Trading.utils.write_to_file import read_json_file
+from Trading.utils.time import get_date_now_cet
+from Trading.instrument.instrument import Instrument
+
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+import logging
+from time import sleep
+
+TIMEFRAME_TO_MONITOR = '1M'
+N_TIMEFRAMES = 12
+
+PRICES_ABOVE_ALERTS = [('USDHUF', 360), ('EURMXN', 21), ('CHFHUF', 390), ('GOLD', 1950)]
+PRICES_BELOW_ALERTS = [('EBAY.US_9', 39), ('NATGAS', 2), ('ETSY.US_9', 79), ('BABA.US_9', 78)]
+
+if __name__ == '__main__':
+
+    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=FORMAT)
+
+    MAIN_LOGGER = logging.getLogger('Main logger')
+    MAIN_LOGGER.setLevel(logging.DEBUG)
+    MAIN_LOGGER.propagate = True
+
+    load_dotenv()
+    username = os.getenv("XTB_USERNAME")
+    password = os.getenv("XTB_PASSWORD")
+    mode = os.getenv("XTB_MODE")
+    client = XTBTradingClient(USERNAME, PASSWORD, MODE, False)
+
+    all_symbols_dict = read_json_file('data/symbols/all_symbols.json')
+
+    report = ""
+    for symbol, price in PRICES_BELOW_ALERTS:
+        try:
+            r = is_symbol_price_below_value(client, symbol, price)
+            if r is not None:
+                report += r
+                MAIN_LOGGER.info(r)
+        except Exception as e:
+            MAIN_LOGGER.info(e)
+            pass
+
+    for symbol, price in PRICES_ABOVE_ALERTS:
+        try:
+            r = is_symbol_price_above_value(client, symbol, price)
+            if r is not None:
+                report += r
+                MAIN_LOGGER.info(r)
+        except Exception as e:
+            MAIN_LOGGER.info(e)
+            pass
+
+    for symbol in all_symbols_dict:
+        try:
+            r = is_symbol_price_below_last_n_intervals_low(client, Instrument(symbol, TIMEFRAME_TO_MONITOR), N_TIMEFRAMES)
+            if r is not None:
+                report += r
+                MAIN_LOGGER.info(r)
+        except Exception as e:
+            MAIN_LOGGER.info(e)
+            pass
+        try:
+            r = is_symbol_price_above_last_n_intervals_low(client, Instrument(symbol, TIMEFRAME_TO_MONITOR), N_TIMEFRAMES)
+            if r is not None:
+                report += r
+                MAIN_LOGGER.info(r)
+        except Exception as e:
+            MAIN_LOGGER.info(e)
+            pass
+
+
+time_now = str(get_date_now_cet())
+subject = "Daily market monitor report " + time_now
+send_email(subject, report)
