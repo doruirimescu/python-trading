4938ac8daecbf7174d76821d087bc3af05d5e91d
Doru Irimescu <>
2023-02-21 19:50:56 +0200

    add sek nok

--- Trading/live/scripts/currency_pair_correlation.py
+++ Trading/live/scripts/currency_pair_correlation.py
@@ -2,24 +2,38 @@ from Trading.live.client.client import XTBLoggingClient
 from Trading.instrument.instrument import Instrument
 from Trading.config.config import USERNAME, PASSWORD, MODE
 from Trading.utils.calculations import calculate_correlation, calculate_rolling_correlation
+from Trading.utils.time import get_datetime_now_cet
+from Trading.config.config import TIMEZONE
+import pytz
+import datetime
+
 import logging
 from Trading.utils.write_to_file import write_to_json_file, read_json_file
-from datetime import datetime
+# from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 from Trading.algo.indicators.indicator import BollingerBandsIndicator
+import sys
 
-N_CANDLES = 360
-PAIR_1_SYMBOL = 'AUDUSD'
-PAIR_2_SYMBOL = 'NZDUSD'
+N_CANDLES = 0
+PAIR_1_SYMBOL = 'USDSEK'
+PAIR_2_SYMBOL = 'USDNOK'
 PAIR_1_POSITION = 'BUY'
 PAIR_2_POSITION = 'SELL'
 PAIR_1_VOLUME = 0.01
 PAIR_2_VOLUME = 0.01
 
-PAIR_1_MULTIPLIER = 1.5
-PAIR_2_MULTIPLIER = 1
+PAIR_1_MULTIPLIER = 1
+PAIR_2_MULTIPLIER = 2
 
+FROM_CLIENT = False
+SAVE_TO_FILE = False
+
+if FROM_CLIENT:
+    PAIR_1_MULTIPLIER, PAIR_2_MULTIPLIER = 1, 1
+
+def exit():
+    sys.exit(0)
 
 def get_cmd(position: str):
     if position == 'BUY':
@@ -30,17 +44,53 @@ def get_cmd(position: str):
 def get_filename():
     return "data/hedging_correlation/" + PAIR_1_SYMBOL + "_" + PAIR_2_SYMBOL + ".json"
 
-def get_prices_from_client(client, should_write_to_file=False):
+def add_missing_candles_to_existing_json():
+    filename = get_filename()
+    json_dict = read_json_file(filename)
+    today_date = get_datetime_now_cet()
+    last_date_in_dict = datetime.datetime.strptime(json_dict['dates'][-1], "%Y-%m-%d %H:%M:%S")
+    last_date_in_dict = pytz.timezone(TIMEZONE).localize(last_date_in_dict)
+
+    days_behind = (today_date - last_date_in_dict).days
+    if days_behind <= 0:
+        return
+
+    pair_1 = client.get_last_n_candles_history(Instrument(PAIR_1_SYMBOL, '1D'), days_behind)
+    pair_2 = client.get_last_n_candles_history(Instrument(PAIR_2_SYMBOL, '1D'), days_behind)
+
+    pair_1_open_price = json_dict[PAIR_1_SYMBOL][0]
+    pair_2_open_price = json_dict[PAIR_2_SYMBOL][0]
+    net_profits = list()
+    pair_1_profits = list()
+    pair_2_profits = list()
+    i = 1
+    for pair_1_o, pair_2_o in zip(pair_1['open'], pair_2['open']):
+        pair_1_profit = client.get_profit_calculation(PAIR_1_SYMBOL, pair_1_open_price, pair_1_o, PAIR_1_VOLUME, get_cmd(PAIR_1_POSITION))
+        pair_2_profit = client.get_profit_calculation(PAIR_2_SYMBOL, pair_2_open_price, pair_2_o, PAIR_2_VOLUME, get_cmd(PAIR_2_POSITION))
+        net_profits.append(pair_1_profit + pair_2_profit)
+        pair_1_profits.append(pair_1_profit)
+        pair_2_profits.append(pair_2_profit)
+        print(f"Candles processed {i} / {days_behind}")
+        i += 1
+
+    json_dict[PAIR_1_SYMBOL] += pair_1['open']
+    json_dict[PAIR_2_SYMBOL] += pair_2['open']
+    json_dict['net_profits'] += net_profits
+    json_dict[PAIR_1_SYMBOL + "_profits"] += pair_1_profits
+    json_dict[PAIR_2_SYMBOL + "_profits"] += pair_2_profits
+    json_dict['N_DAYS'] = int(json_dict['N_DAYS']) + days_behind
+    json_dict['dates'] += pair_1['date']
+
+    write_to_json_file(get_filename(), json_dict)
+
+def get_prices_from_client(client):
     pair_1 = client.get_last_n_candles_history(Instrument(PAIR_1_SYMBOL, '1D'), N_CANDLES)
     pair_2 = client.get_last_n_candles_history(Instrument(PAIR_2_SYMBOL, '1D'), N_CANDLES)
 
-    pair_1_oh = list(zip(pair_1['open'], pair_1['high']))
-    pair_2_oh = list(zip(pair_2['open'], pair_2['high']))
 
-
-    pair_1_open_price = pair_1_oh[0][0]
+    pair_1_open_price = pair_1['open'][0]
     print(f"{PAIR_1_SYMBOL} open price {pair_1_open_price}")
-    pair_2_open_price = pair_2_oh[0][0]
+    pair_2_open_price = pair_2['open'][0]
     print(f"{PAIR_2_SYMBOL} open price {pair_2_open_price}")
 
     net_profits = list()
@@ -56,7 +106,7 @@ def get_prices_from_client(client, should_write_to_file=False):
         pair_1_profits.append(pair_1_profit)
         pair_2_profits.append(pair_2_profit)
 
-    if should_write_to_file:
+    if SAVE_TO_FILE:
         prices = {  PAIR_1_SYMBOL: pair_1['open'],
                     PAIR_2_SYMBOL: pair_2['open'],
                     'net_profits': net_profits,
@@ -67,7 +117,7 @@ def get_prices_from_client(client, should_write_to_file=False):
                     PAIR_1_SYMBOL + "_position": PAIR_1_POSITION,
                     PAIR_2_SYMBOL + "_position": PAIR_2_POSITION,
                     'N_DAYS': N_CANDLES,
-                    'date': str(datetime.now().date())}
+                    'dates': pair_1['date']}
         write_to_json_file(get_filename(), prices)
 
 
@@ -101,9 +151,13 @@ if __name__ == '__main__':
     MAIN_LOGGER.propagate = True
 
     client = XTBLoggingClient(USERNAME, PASSWORD, MODE, False)
+    add_missing_candles_to_existing_json()
+    exit()
 
-    #(pair_1_o, pair_2_o, net_profits) = get_prices_from_client(client, should_write_to_file=True)
-    (pair_1_o, pair_2_o, net_profits) = get_prices_from_file()
+    if FROM_CLIENT:
+        (pair_1_o, pair_2_o, net_profits) = get_prices_from_client(client)
+    else:
+        (pair_1_o, pair_2_o, net_profits) = get_prices_from_file()
 
     avg_net = sum(net_profits) / len(net_profits)
     print(f"Average net profit: {avg_net}")
@@ -126,6 +180,7 @@ if __name__ == '__main__':
     ax[1].plot(net_profits, label=f'Hedged net profit', color='orange')
     ax[2].plot(rolling_correlation, label=f'Rolling correlation', color='green')
     bb.plot(ax[1])
+    ax[0].axhline(color='red')
 
     ax[0].legend()
     ax[2].legend()
@@ -142,4 +197,6 @@ if __name__ == '__main__':
 
     ax[1].set_xlabel('Days')
     ax[1].set_ylabel('Eur')
+    ax[0].tick_params(labelbottom=True)
+
     plt.show()
