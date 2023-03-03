3f7fe9028be8b79e04d8fe286030687ee06176ed
Doru Irimescu <>
2023-03-03 19:58:24 +0200

    Encapsulate get logger

---
+++ Trading/utils/logging.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def get_logger():
+    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=FORMAT)
+
+    MAIN_LOGGER = logging.getLogger('Main logger')
+    MAIN_LOGGER.setLevel(logging.DEBUG)
+    MAIN_LOGGER.propagate = True
+    return MAIN_LOGGER
