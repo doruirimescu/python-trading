1377a166d164a0c073d3a7eebcfe1e5af89f7595
Doru Irimescu <>
2023-06-18 20:18:08 +0300

    Add faster computations, sliders

--- Trading/live/hedge/data.py
+++ Trading/live/hedge/data.py
@@ -7,52 +7,157 @@ from Trading.utils.calculations import calculate_net_profit_eur
 from Trading.live.hedge.fixed_conversion_rates import convert_currency_to_eur
 from functools import partial
 from typing import List
+from dataclasses import dataclass
 
 MAIN_LOGGER = get_logger()
 
 
-def get_prices_from_client(client: LoggingClient,
-                           instrument_1: Instrument,
-                           instrument_2: Instrument,
-                           pair_1_volume: float,
-                           pair_2_volume: float,
-                           n_candles: int,
-                           pair_1_multiplier: float = 1.0,
-                           pair_2_multiplier: float = 1.0,
-                           pair_1_position: str = 'BUY',
-                           pair_2_position: str = 'SELL',
-                           should_reference_profits_to_zero=False,
-                           should_calculate_prices_with_client=True):
+@dataclass
+class PositionInfo:
+    instrument: Instrument
+    volume: int
+    type: str
+    multiplier: int
+    open_prices:[]
+    net_profits:[]
+
+    def get_net_profits_with_multiplier(self):
+        return [x * self.multiplier for x in self.net_profits]
+
+
+def calculate_net_profit_with_multiplier_of_positions(positions: List[PositionInfo]):
+    net_profits = [0] * len(positions[0].net_profits)
+    for position in positions:
+        net_profits = [x + y for x, y in zip(net_profits, position.get_net_profits_with_multiplier())]
+    return net_profits
+
+
+def get_returns_for_list_of_positions(
+    client: LoggingClient,
+    positions: List[PositionInfo],
+    n_candles: int,
+    should_reference_profits_to_zero=False,
+):
+    net_profits = [0] * n_candles
+    dates = []
+
+    for position in positions:
+        instrument = position.instrument
+        symbol = instrument.symbol
+        volume = position.volume
+        position_type = position.type
+        multiplier = position.multiplier
+        info = client.get_symbol(symbol)
+        contract_value = volume * info["contractSize"]
+        conversion_rate_eur = convert_currency_to_eur(info["currencyProfit"])
+
+        profit_calculator = partial(
+            calculate_net_profit_eur,
+            contract_value=contract_value,
+            quote_currency_to_eur_conversion_rate=conversion_rate_eur,
+            cmd=get_cmd(position_type),
+        )
+        history = client.get_last_n_candles_history(instrument, n_candles)
+        if not dates:
+            dates = history["date"]
+
+        open_price = 0
+        if not should_reference_profits_to_zero:
+            open_price = history["open"][0]
+        pair_profits = list()
+
+        for daily_open_price in history["open"]:
+            profit = profit_calculator(
+                open_price=open_price, close_price=daily_open_price
+            )
+            pair_profits.append(multiplier*profit)
+        position.open_prices = history["open"]
+        position.net_profits = pair_profits
+
+        net_profits = [x + y for x, y in zip(net_profits, pair_profits)]
+
+    data = {
+        p.instrument.symbol: p.open_prices for p in positions
+    }
+
+    data["net_profits"] = net_profits
+    data["N_DAYS"] = n_candles
+
+    profits = {
+        p.instrument.symbol + '_profits': p.net_profits for p in positions
+    }
+    data.update(profits)
+    volumes = {
+        p.instrument.symbol + '_volume': p.volume for p in positions
+    }
+    data.update(volumes)
+    positions = {
+        p.instrument.symbol + '_position': p.type for p in positions
+    }
+    data.update(positions)
+    data['date'] = dates
+
+    return data
+
+
+def get_prices_from_client(
+    client: LoggingClient,
+    instrument_1: Instrument,
+    instrument_2: Instrument,
+    pair_1_volume: float,
+    pair_2_volume: float,
+    n_candles: int,
+    pair_1_multiplier: float = 1.0,
+    pair_2_multiplier: float = 1.0,
+    pair_1_position: str = "BUY",
+    pair_2_position: str = "SELL",
+    should_reference_profits_to_zero=False,
+    should_calculate_prices_with_client=True,
+):
     pair_1_symbol = instrument_1.symbol
     pair_2_symbol = instrument_2.symbol
 
     if should_calculate_prices_with_client:
         pair_1_profit_calculator = partial(
-            client.get_profit_calculation, symbol=pair_1_symbol, volume=pair_1_volume, cmd=get_cmd(pair_1_position))
+            client.get_profit_calculation,
+            symbol=pair_1_symbol,
+            volume=pair_1_volume,
+            cmd=get_cmd(pair_1_position),
+        )
         pair_2_profit_calculator = partial(
-            client.get_profit_calculation, symbol=pair_2_symbol, volume=pair_2_volume, cmd=get_cmd(pair_2_position))
+            client.get_profit_calculation,
+            symbol=pair_2_symbol,
+            volume=pair_2_volume,
+            cmd=get_cmd(pair_2_position),
+        )
     else:
         info_1 = client.get_symbol(pair_1_symbol)
         info_2 = client.get_symbol(pair_2_symbol)
-        pair_1_contract_value = pair_1_volume * info_1['contractSize']
-        pair_2_contract_value = pair_2_volume * info_2['contractSize']
-        pair_1_conversion_rate_eur = convert_currency_to_eur(info_1['currencyProfit'])
-        pair_2_conversion_rate_eur = convert_currency_to_eur(info_2['currencyProfit'])
+        pair_1_contract_value = pair_1_volume * info_1["contractSize"]
+        pair_2_contract_value = pair_2_volume * info_2["contractSize"]
+        pair_1_conversion_rate_eur = convert_currency_to_eur(info_1["currencyProfit"])
+        pair_2_conversion_rate_eur = convert_currency_to_eur(info_2["currencyProfit"])
 
         pair_1_profit_calculator = partial(
-            calculate_net_profit_eur, contract_value=pair_1_contract_value,
-            quote_currency_to_eur_conversion_rate=pair_1_conversion_rate_eur, cmd=get_cmd(pair_1_position))
+            calculate_net_profit_eur,
+            contract_value=pair_1_contract_value,
+            quote_currency_to_eur_conversion_rate=pair_1_conversion_rate_eur,
+            cmd=get_cmd(pair_1_position),
+        )
         pair_2_profit_calculator = partial(
-            calculate_net_profit_eur, contract_value=pair_2_contract_value,
-            quote_currency_to_eur_conversion_rate=pair_2_conversion_rate_eur, cmd=get_cmd(pair_2_position))
+            calculate_net_profit_eur,
+            contract_value=pair_2_contract_value,
+            quote_currency_to_eur_conversion_rate=pair_2_conversion_rate_eur,
+            cmd=get_cmd(pair_2_position),
+        )
 
     pair_1 = client.get_last_n_candles_history(instrument_1, n_candles)
     pair_2 = client.get_last_n_candles_history(instrument_2, n_candles)
 
     pair_1_open_price, pair_2_open_price = 0, 0
     if not should_reference_profits_to_zero:
-        pair_1_open_price = pair_1['open'][0]
-        pair_2_open_price = pair_2['open'][0]
+        pair_1_open_price = pair_1["open"][0]
+        pair_2_open_price = pair_2["open"][0]
     MAIN_LOGGER.info(f"{pair_1_symbol} open price {pair_1_open_price}")
     MAIN_LOGGER.info(f"{pair_2_symbol} open price {pair_2_open_price}")
 
@@ -61,45 +166,53 @@ def get_prices_from_client(client: LoggingClient,
     pair_2_profits = list()
     i = 1
 
-    for pair_1_o, pair_2_o in zip(pair_1['open'], pair_2['open']):
-        pair_1_profit = pair_1_profit_calculator(open_price=pair_1_open_price, close_price=pair_1_o)
-        pair_2_profit = pair_2_profit_calculator(open_price=pair_2_open_price, close_price=pair_2_o)
+    for pair_1_o, pair_2_o in zip(pair_1["open"], pair_2["open"]):
+        pair_1_profit = pair_1_profit_calculator(
+            open_price=pair_1_open_price, close_price=pair_1_o
+        )
+        pair_2_profit = pair_2_profit_calculator(
+            open_price=pair_2_open_price, close_price=pair_2_o
+        )
 
-        net_profits.append(pair_1_multiplier * pair_1_profit + pair_2_multiplier * pair_2_profit)
+        net_profits.append(
+            pair_1_multiplier * pair_1_profit + pair_2_multiplier * pair_2_profit
+        )
         pair_1_profits.append(pair_1_profit)
         pair_2_profits.append(pair_2_profit)
         MAIN_LOGGER.info(f"Candles processed {i} / {n_candles}")
         i += 1
 
     prices = {
-        pair_1_symbol: pair_1['open'],
-        pair_2_symbol: pair_2['open'],
-        'net_profits': net_profits,
+        pair_1_symbol: pair_1["open"],
+        pair_2_symbol: pair_2["open"],
+        "net_profits": net_profits,
         pair_1_symbol + "_profits": pair_1_profits,
         pair_2_symbol + "_profits": pair_2_profits,
         pair_1_symbol + "_volume": pair_1_volume,
         pair_2_symbol + "_volume": pair_2_volume,
         pair_1_symbol + "_position": pair_1_position,
         pair_2_symbol + "_position": pair_2_position,
-        'N_DAYS': n_candles,
-        'dates': pair_1['date']
+        "N_DAYS": n_candles,
+        "dates": pair_1["date"],
     }
 
     return prices
 
 
 def get_prices_from_file(
-        pair_1_symbol: str,
-        pair_2_symbol: str,
-        pair_1_multiplier: float,
-        pair_2_multiplier: float,
+    pair_1_symbol: str,
+    pair_2_symbol: str,
+    pair_1_multiplier: float,
+    pair_2_multiplier: float,
 ):
     filename = get_filename(pair_1_symbol, pair_2_symbol)
     json_dict = read_json_file(filename)
     pair_1_o = json_dict[pair_1_symbol]
     pair_2_o = json_dict[pair_2_symbol]
     net_profits = list()
-    for p1, p2 in zip(json_dict[pair_1_symbol + "_profits"], json_dict[pair_2_symbol + "_profits"]):
+    for p1, p2 in zip(
+        json_dict[pair_1_symbol + "_profits"], json_dict[pair_2_symbol + "_profits"]
+    ):
         net_profits.append(pair_1_multiplier * p1 + pair_2_multiplier * p2)
 
     pair_1_open_price = pair_1_o[0]
@@ -111,11 +224,7 @@ def get_prices_from_file(
     return (pair_1_o, pair_2_o, net_profits)
 
 
-def get_filename(
-        pair_1_symbol: str,
-        pair_2_symbol: str,
-        extra: str = ""
-):
+def get_filename(pair_1_symbol: str, pair_2_symbol: str, extra: str = ""):
     return f"{DATA_STORAGE_PATH}hedging_correlation/{pair_1_symbol}_{pair_2_symbol}{extra}.json"
 
 
--- Trading/live/scripts/improved_currency_pair_correlation.py
+++ Trading/live/scripts/improved_currency_pair_correlation.py
@@ -1,30 +1,47 @@
 from Trading.live.client.client import XTBLoggingClient, get_cmd
 from Trading.instrument.instrument import Instrument
 from Trading.config.config import USERNAME, PASSWORD, MODE
-from Trading.utils.calculations import calculate_correlation, calculate_rolling_correlation, count_zero_crossings
+from Trading.utils.calculations import (
+    calculate_correlation,
+    calculate_rolling_correlation,
+    count_zero_crossings,
+)
 from Trading.utils.logging import get_logger
 from Trading.utils.time import get_datetime_now_cet
 from Trading.config.config import TIMEZONE
-from Trading.live.hedge.data import get_filename, get_prices_from_client, get_prices_from_file
+from Trading.live.hedge.data import (
+    get_filename,
+    get_prices_from_client,
+    calculate_net_profit_with_multiplier_of_positions,
+    PositionInfo,
+    get_returns_for_list_of_positions,
+)
+from matplotlib.widgets import Slider, Button, RadioButtons
 
 import pytz
 import datetime
 
 from Trading.utils.write_to_file import write_to_json_file, read_json_file
+
 # from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 from Trading.algo.indicators.indicator import BollingerBandsIndicator
 import sys
 
+
+def exit():
+    sys.exit(0)
+
+
 #! Use percentage of the initial price as return. In this way, both net profits are
 #! already normalized
 
-N_CANDLES = 310
-PAIR_1_SYMBOL = 'AUDCHF'
-PAIR_2_SYMBOL = 'NZDCHF'
-PAIR_1_POSITION = 'BUY'
-PAIR_2_POSITION = 'SELL'
+N_CANDLES = 150
+PAIR_1_SYMBOL = "NZDUSD"
+PAIR_2_SYMBOL = "AUDUSD"
+PAIR_1_POSITION = "SELL"
+PAIR_2_POSITION = "BUY"
 PAIR_1_VOLUME = 0.01
 PAIR_2_VOLUME = 0.01
 
@@ -33,8 +50,41 @@ PAIR_2_MULTIPLIER = 1
 
 FILENAME = get_filename(PAIR_1_SYMBOL, PAIR_2_SYMBOL)
 
-def exit():
-    sys.exit(0)
+
+positions = [
+    PositionInfo(
+        instrument=Instrument(PAIR_1_SYMBOL, "1D"),
+        volume=PAIR_1_VOLUME,
+        type=PAIR_1_POSITION,
+        multiplier=PAIR_1_MULTIPLIER,
+        open_prices=[],
+        net_profits=[],
+    ),
+    PositionInfo(
+        instrument=Instrument(PAIR_2_SYMBOL, "1D"),
+        volume=PAIR_2_VOLUME,
+        type=PAIR_2_POSITION,
+        multiplier=PAIR_2_MULTIPLIER,
+        open_prices=[],
+        net_profits=[],
+    ),
+    PositionInfo(
+        instrument=Instrument("EURUSD", "1D"),
+        volume=0.01,
+        type="SELL",
+        multiplier=1,
+        open_prices=[],
+        net_profits=[],
+    ),
+    PositionInfo(
+        instrument=Instrument("OIL", "1D"),
+        volume=0.01,
+        type="SELL",
+        multiplier=1,
+        open_prices=[],
+        net_profits=[],
+    ),
+]
 
 
 def add_missing_candles_to_existing_json():
@@ -42,7 +92,9 @@ def add_missing_candles_to_existing_json():
     if not json_dict:
         return
     today_date = get_datetime_now_cet()
-    last_date_in_dict = datetime.datetime.strptime(json_dict['dates'][-1], "%Y-%m-%d %H:%M:%S")
+    last_date_in_dict = datetime.datetime.strptime(
+        json_dict["dates"][-1], "%Y-%m-%d %H:%M:%S"
+    )
     last_date_in_dict_no_timezone = last_date_in_dict
     last_date_in_dict = pytz.timezone(TIMEZONE).localize(last_date_in_dict)
 
@@ -51,8 +103,12 @@ def add_missing_candles_to_existing_json():
     if days_behind <= 0:
         return
 
-    pair_1 = client.get_last_n_candles_history(Instrument(PAIR_1_SYMBOL, '1D'), days_behind)
-    pair_2 = client.get_last_n_candles_history(Instrument(PAIR_2_SYMBOL, '1D'), days_behind)
+    pair_1 = client.get_last_n_candles_history(
+        Instrument(PAIR_1_SYMBOL, "1D"), days_behind
+    )
+    pair_2 = client.get_last_n_candles_history(
+        Instrument(PAIR_2_SYMBOL, "1D"), days_behind
+    )
 
     pair_1_open_price = json_dict[PAIR_1_SYMBOL][0]
     pair_2_open_price = json_dict[PAIR_2_SYMBOL][0]
@@ -60,9 +116,21 @@ def add_missing_candles_to_existing_json():
     pair_1_profits = list()
     pair_2_profits = list()
     i = 1
-    for pair_1_o, pair_2_o in zip(pair_1['open'], pair_2['open']):
-        pair_1_profit = client.get_profit_calculation(PAIR_1_SYMBOL, pair_1_open_price, pair_1_o, PAIR_1_VOLUME, get_cmd(PAIR_1_POSITION))
-        pair_2_profit = client.get_profit_calculation(PAIR_2_SYMBOL, pair_2_open_price, pair_2_o, PAIR_2_VOLUME, get_cmd(PAIR_2_POSITION))
+    for pair_1_o, pair_2_o in zip(pair_1["open"], pair_2["open"]):
+        pair_1_profit = client.get_profit_calculation(
+            PAIR_1_SYMBOL,
+            pair_1_open_price,
+            pair_1_o,
+            PAIR_1_VOLUME,
+            get_cmd(PAIR_1_POSITION),
+        )
+        pair_2_profit = client.get_profit_calculation(
+            PAIR_2_SYMBOL,
+            pair_2_open_price,
+            pair_2_o,
+            PAIR_2_VOLUME,
+            get_cmd(PAIR_2_POSITION),
+        )
         net_profits.append(pair_1_profit + pair_2_profit)
         pair_1_profits.append(pair_1_profit)
         pair_2_profits.append(pair_2_profit)
@@ -70,23 +138,23 @@ def add_missing_candles_to_existing_json():
         i += 1
 
     MAIN_LOGGER.info(last_date_in_dict_no_timezone)
-    if last_date_in_dict_no_timezone == pair_1['date'][0]:
+    if last_date_in_dict_no_timezone == pair_1["date"][0]:
         MAIN_LOGGER.info("POPPING")
-        pair_1['open'].pop()
-        pair_2['open'].pop()
+        pair_1["open"].pop()
+        pair_2["open"].pop()
         net_profits.pop()
         json_dict[PAIR_1_SYMBOL + "_profits"].pop()
         json_dict[PAIR_2_SYMBOL + "_profits"].pop()
-        json_dict['dates'].pop()
+        json_dict["dates"].pop()
         days_behind -= 1
 
-    json_dict[PAIR_1_SYMBOL] += pair_1['open']
-    json_dict[PAIR_2_SYMBOL] += pair_2['open']
-    json_dict['net_profits'] += net_profits
+    json_dict[PAIR_1_SYMBOL] += pair_1["open"]
+    json_dict[PAIR_2_SYMBOL] += pair_2["open"]
+    json_dict["net_profits"] += net_profits
     json_dict[PAIR_1_SYMBOL + "_profits"] += pair_1_profits
     json_dict[PAIR_2_SYMBOL + "_profits"] += pair_2_profits
-    json_dict['N_DAYS'] = int(json_dict['N_DAYS']) + days_behind
-    json_dict['dates'] += pair_1['date']
+    json_dict["N_DAYS"] = int(json_dict["N_DAYS"]) + days_behind
+    json_dict["dates"] += pair_1["date"]
 
     write_to_json_file(FILENAME, json_dict)
     exit()
@@ -96,78 +164,119 @@ def convert_ron_to_eur(net_profits):
     """Profit calculations coming from client are in RON, because the demo account is in RON.
     Thus, before presenting, we need to convert in eur
     """
-    eur_ron = float(client.get_current_price('EURRON')[0])
+    eur_ron = float(client.get_current_price("EURRON")[0])
     MAIN_LOGGER.info(eur_ron)
-    return [float(n)/eur_ron for n in net_profits]
+    return [float(n) / eur_ron for n in net_profits]
 
 
-if __name__ == '__main__':
+def plot_main(main_ax, positions, main_fig):
+
+    for ax in main_ax:
+        ax.cla()
+
+    net_profits = calculate_net_profit_with_multiplier_of_positions(positions)
+    avg_net = sum(net_profits) / len(net_profits)
+    MAIN_LOGGER.info(f"Average net profit: {avg_net}")
+
+    main_ax[0].plot(net_profits, label=f"Hedged net profit", color="green")
+    main_ax[0].axhline(color="red", y=avg_net)
+
+    main_ax[0].legend()
+    main_ax[0].grid()
+
+    txt = ""
+    for position in positions:
+        txt += position.instrument.symbol
+        txt += " "
+        txt += str(position.type) + " "
+        txt += str(position.volume * position.multiplier) + " "
+        txt += "with "
+    txt = txt[:-6]
+
+    main_ax[0].set_title(f"Hedged net profit for {txt}")
+    main_ax[0].set_xlabel("Days")
+    main_ax[0].set_ylabel("Eur")
+    main_ax[0].tick_params(labelbottom=True)
+
+    # Prepare different colors
+    colors = ["red", "blue", "green", "orange", "purple", "black"]
+    for i, position in enumerate(positions):
+        main_ax[1 + i].plot(
+            position.get_net_profits_with_multiplier(),
+            label=f"{position.instrument.symbol} profit",
+            color=colors[i],
+        )
+        main_ax[1 + i].legend()
+        main_ax[1 + i].grid()
+    main_fig.canvas.draw()
+
+if __name__ == "__main__":
     MAIN_LOGGER = get_logger()
 
     client = XTBLoggingClient(USERNAME, PASSWORD, MODE, False)
-    #add_missing_candles_to_existing_json()
+    # add_missing_candles_to_existing_json()
 
-    data = get_prices_from_client(
-        client,
-        Instrument(PAIR_1_SYMBOL, '1D'),
-        Instrument(PAIR_2_SYMBOL, '1D'),
-        0.01,
-        0.01,
-        N_CANDLES,
-        PAIR_1_MULTIPLIER,
-        PAIR_2_MULTIPLIER,
-        PAIR_1_POSITION,
-        PAIR_2_POSITION,
-        should_reference_profits_to_zero=False,
-        should_calculate_prices_with_client=False)
-    (pair_1_o, pair_2_o, net_profits) = data[PAIR_1_SYMBOL], data[PAIR_2_SYMBOL], data['net_profits']
+    data = get_returns_for_list_of_positions(client, positions, N_CANDLES)
+    net_profits = data["net_profits"]
+    pair_1_o = data[PAIR_1_SYMBOL]
+    pair_2_o = data[PAIR_2_SYMBOL]
+
     write_to_json_file(FILENAME, data)
 
+    correlation = calculate_correlation(
+        PAIR_1_SYMBOL, PAIR_2_SYMBOL, pair_1_o, pair_2_o
+    )
 
-    avg_net = sum(net_profits) / len(net_profits)
-    MAIN_LOGGER.info(f"Average net profit: {avg_net}")
-    correlation = calculate_correlation(PAIR_1_SYMBOL, PAIR_2_SYMBOL,
-                                        pair_1_o, pair_2_o)
+    rolling_correlation = calculate_rolling_correlation(
+        PAIR_1_SYMBOL, PAIR_2_SYMBOL, pair_1_o, pair_2_o, 20
+    )
 
     # Prepare net profits for bollinger bands
-    main_data = pd.DataFrame({'close': net_profits})
+    main_data = pd.DataFrame({"close": net_profits})
     bb = BollingerBandsIndicator(20)
     bb.calculate_bb(main_data)
 
-    rolling_correlation = calculate_rolling_correlation(PAIR_1_SYMBOL,
-                                                        PAIR_2_SYMBOL,
-                                                        pair_1_o,
-                                                        pair_2_o,
-                                                        20)
-
     MAIN_LOGGER.info(f"Crossed zero {count_zero_crossings(net_profits)} times")
 
-    from statsmodels.tsa.stattools import acf, q_stat, adfuller
-    ADF = adfuller(net_profits)[1]
-    print(f"ADF is: {ADF}")
+    # from statsmodels.tsa.stattools import acf, q_stat, adfuller
+    # ADF = adfuller(net_profits)[1]
+    # print(f"ADF is: {ADF}")
 
-    fig, ax = plt.subplots(3, figsize=(10, 5), sharex=True)
-    ax[0].plot(net_profits, label=f'Hedged net profit', color='green')
+    main_fig, main_ax = plt.subplots(1 + len(positions), figsize=(10, 5), sharex=True)
+    plot_main(main_ax, positions, main_fig)
 
-    ax[1].plot(net_profits, label=f'Hedged net profit', color='orange')
-    bb.plot(ax[1])
-    ax[1].set_title(f'Bollinger bands on hedged net profit')
-    ax[1].set_xlabel('Days')
-    ax[1].set_ylabel('Eur')
-    ax[1].grid()
+    fig = plt.figure(figsize=(10, 5))
+    sliders = []
+    for i, position in enumerate(positions):
+        sliders.append(
+            Slider(
+                plt.axes([0.25, 0.2 + i * 0.2, 0.5, 0.03]),
+                f"{position.instrument.symbol} multiplier",
+                0,
+                10,
+                valinit=position.multiplier,
+                valstep=1,
+                dragging=True,
+            )
+        )
 
-    ax[0].axhline(color='red', y=avg_net)
+    for i, slider in enumerate(sliders):
+        def update(val, index=i):
+            print(f"---- Updating with value {val}----")
+            positions[index].multiplier = val
+            plot_main(main_ax, positions, main_fig)
+            for p in positions:
+                print(f"{p.instrument.symbol} multiplier: {p.multiplier}")
+        sliders[i].on_changed(update)
 
-    ax[0].legend()
-    ax[0].grid()
-    ax[0].set_title(f'Hedged net profit for {PAIR_1_SYMBOL} {PAIR_1_POSITION} {PAIR_1_MULTIPLIER * PAIR_1_VOLUME} '
-                    f'with {PAIR_2_SYMBOL} {PAIR_2_POSITION} {PAIR_2_MULTIPLIER * PAIR_2_VOLUME}')
-    ax[0].set_xlabel('Days')
-    ax[0].set_ylabel('Eur')
-    ax[0].tick_params(labelbottom=True)
-
-    ax[2].plot(rolling_correlation, label=f'Rolling correlation', color='green')
-    ax[2].legend()
-    ax[2].grid()
-
+    # create another plot for bollinger bands
+    fig, ax = plt.subplots(1, figsize=(10, 5), sharex=True)
+    ax.plot(net_profits, label=f"Hedged net profit", color="orange")
+    bb.plot(ax)
+    ax.set_title(f"Bollinger bands on hedged net profit")
+    ax.set_xlabel("Days")
+    ax.set_ylabel("Eur")
+    ax.grid()
+    # add slider for bollinger bands
+    ax_bb = plt.axes([0.25, 0.1, 0.65, 0.03])
     plt.show()
