a0d8c22a9da752bbe0930104687532672ca22056
Doru Irimescu <>
2023-08-30 14:12:37 +0300

    Format

--- Trading/database/add_contract_value_into_database.py
+++ Trading/database/add_contract_value_into_database.py
@@ -14,14 +14,13 @@ def delete_contract_value():
     cursor.execute(query)
     db.close()
 
+
 def add_contract_value(symbol, value):
     # Open database connection
     db = pymysql.connect(host='localhost', database=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD)
     db.autocommit(True)
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
-
-
 
     query = (
         f"INSERT INTO trading.contract_value(symbol, contract_value) VALUES ('{symbol}', {value});"
