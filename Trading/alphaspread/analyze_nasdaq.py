c27a4763068319eddc384b73b84ce8865b9a4554
Doru Irimescu <>
2023-10-02 15:28:29 +0300

    Add fix to use json data

--- Trading/alphaspread/analyze_nasdaq.py
+++ Trading/alphaspread/analyze_nasdaq.py
@@ -3,6 +3,7 @@ from typing import List
 import requests
 from alphaspread import analyze_url
 from constants import NASDAQ_ANALYSIS_FILENAME
+from time import sleep
 
 def get_nasdaq_symbols() -> List:
     headers = {
@@ -21,16 +22,39 @@ def get_nasdaq_symbols() -> List:
 if __name__ == "__main__":
     nasdaq_symbols = get_nasdaq_symbols()
     undervalued_symbols = []
+
+    with open(NASDAQ_ANALYSIS_FILENAME, "r") as f:
+        # load json
+        json_str = f.read()
+        data = json.loads(json_str)
+
+
     for symbol in nasdaq_symbols:
+        # Check if symbol is already in json
+        should_continue = False
+        for d in data:
+            if symbol in d["symbol"]:
+                print(f"Skipping {symbol}")
+                should_continue = True
+                break
+        if should_continue:
+            continue
+
         url = f"https://www.alphaspread.com/security/nasdaq/{symbol}/summary"
         try:
             analysis = analyze_url(url, symbol)
             undervalued_symbols.append(analysis)
+
+            # add analysis to json
+            data.append(analysis.dict())
+
         except Exception as e:
             print(f"Error analyzing {symbol}: {e}")
 
+        sleep(3.0)
+
     with open(NASDAQ_ANALYSIS_FILENAME, "w") as f:
         json_str = json.dumps(
-            [analysis.dict() for analysis in undervalued_symbols], indent=4
+            data, indent=4
         )
         f.write(json_str)
