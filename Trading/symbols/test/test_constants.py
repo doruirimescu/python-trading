e2e6049d21c2a3b21b75c06c36a0957bfb40b0d8
Doru Irimescu <>
2023-10-05 20:40:36 +0300

    Add tests

--- Trading/symbols/test/test_constants.py
+++ Trading/symbols/test/test_constants.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
-from Trading.symbols.constants import XTB_ALL_SYMBOLS
+from Trading.symbols.constants import (
+    XTB_ALL_SYMBOLS, XTB_STOCK_SYMBOLS, XTB_STOCK_TICKERS)
 
 class TestConstants(TestCase):
     def test_c(self):
         self.assertGreaterEqual(3862, len(XTB_ALL_SYMBOLS))
+        self.assertIn("AAPL.US_9", XTB_STOCK_SYMBOLS)
+        self.assertIn("AAPL", XTB_STOCK_TICKERS)
