dafba6c670f322b7d691e1cc2afbc255d0b5ceb9
Doru Irimescu <>
2023-10-05 20:35:04 +0300

    Update with failing symbols

---
+++ Trading/symbols/xtb/update_with_failing_symbols.py
@@ -0,0 +1,21 @@
+import json
+
+from Trading.symbols.constants import (
+    XTB_ALL_SYMBOLS_PATH,
+    XTB_ALL_SYMBOLS_DICT,
+    XTB_FAILING_SYMBOLS,
+    XTB_STOCK_SYMBOLS_DICT,
+    XTB_STOCK_SYMBOLS_PATH
+)
+
+for symbol in XTB_FAILING_SYMBOLS:
+    if symbol in XTB_ALL_SYMBOLS_DICT:
+        XTB_ALL_SYMBOLS_DICT.pop(symbol)
+    if symbol in XTB_STOCK_SYMBOLS_DICT:
+        XTB_STOCK_SYMBOLS_DICT.pop(symbol)
+
+with open(XTB_ALL_SYMBOLS_PATH, 'w') as f:
+    json.dump(XTB_ALL_SYMBOLS_DICT, f, indent=4)
+
+with open(XTB_STOCK_SYMBOLS_PATH, 'w') as f:
+    json.dump(XTB_STOCK_SYMBOLS_DICT, f, indent=4)
