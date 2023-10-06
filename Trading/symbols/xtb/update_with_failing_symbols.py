4af8a4bfe6a5c07badd7d14c015f3d638c19d255
Doru Irimescu <>
2023-10-06 13:47:31 +0300

    Remove close only stocks

--- Trading/symbols/xtb/update_with_failing_symbols.py
+++ Trading/symbols/xtb/update_with_failing_symbols.py
@@ -19,6 +19,14 @@ for symbol in XTB_FAILING_SYMBOLS:
     if symbol in XTB_ETF_SYMBOLS_DICT:
         XTB_ETF_SYMBOLS_DICT.pop(symbol)
 
+stock_symbols = [s for s in XTB_STOCK_SYMBOLS_DICT]
+for symbol in stock_symbols:
+    if ('close only' in XTB_STOCK_SYMBOLS_DICT[symbol]['description'] or
+        "CLOSE ONLY" in XTB_STOCK_SYMBOLS_DICT[symbol]['description'] or
+        "Close only" in XTB_STOCK_SYMBOLS_DICT[symbol]['description']):
+        XTB_STOCK_SYMBOLS_DICT.pop(symbol)
+        print(symbol)
+
 with open(XTB_ALL_SYMBOLS_PATH, 'w') as f:
     json.dump(XTB_ALL_SYMBOLS_DICT, f, indent=4)
 
