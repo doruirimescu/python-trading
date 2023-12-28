17c3b3f538b21f6f3642902c4773d83e8267b4d9
Doru Irimescu <>
2023-12-28 13:50:01 +0200

    Fix path

--- script/daily_workflow.sh
+++ script/daily_workflow.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Nasdaq analysis
-python3 ../Trading/stock/analyze_nasdaq.py --nasdaq
-python3 ../Trading/stock/visualize.py --nasdaq --save
+python3 Trading/stock/analyze_nasdaq.py --nasdaq
+python3 Trading/stock/visualize.py --nasdaq --save
