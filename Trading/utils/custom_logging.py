a10a442a97d35b3d17ba7111b3e160911bd983fc
Doru Irimescu <>
2024-04-14 19:35:56 +0300

    Update logging name

--- Trading/utils/custom_logging.py
+++ Trading/utils/custom_logging.py
@@ -1,11 +1,11 @@
 import logging
 
 
-def get_logger():
+def get_logger(name: str = 'Main logger'):
     FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(format=FORMAT)
 
-    MAIN_LOGGER = logging.getLogger('Main logger')
+    MAIN_LOGGER = logging.getLogger(name)
     MAIN_LOGGER.setLevel(logging.DEBUG)
     MAIN_LOGGER.propagate = True
     return MAIN_LOGGER
