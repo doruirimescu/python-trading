6272e91789b4f421f24c7ce933024968a7b3ce73
Doru Irimescu <>
2024-04-14 19:39:58 +0300

    Update google search

--- Trading/utils/google_search.py
+++ Trading/utils/google_search.py
@@ -3,12 +3,23 @@ from Trading.utils.custom_logging import get_logger
 
 LOGGER = get_logger(__name__)
 class GoogleSearchFailed(Exception):
-    def __init__(self, query):
+    def __init__(self, query: str):
         message = f"Google search failed. Query: {query}"
         LOGGER.error(message)
         super().__init__(message)
 
-def get_first_google_result(query):
+def get_first_google_result(query: str) -> str:
+    """Google search for query and return the first result
+
+    Args:
+        query (str): The query to search for
+
+    Raises:
+        GoogleSearchFailed: If the search fails
+
+    Returns:
+        str: The first result of the search
+    """
     try:
         search_results = search(query, num_results=1)
         first_result = next(search_results)
