1bea4bd32d2c729f622da585122c24dd57964a3c
Doru Irimescu <>
2024-04-14 16:40:18 +0300

    Add Timeframe class

---
+++ Trading/instrument/test/test_timeframe.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from Trading.instrument.timeframes import Timeframe, TIMEFRAMES
+
+
+class TestTimeframe(TestCase):
+    def test_timeframe(self):
+        for t in TIMEFRAMES:
+            tf = Timeframe(t)
+            self.assertIsInstance(tf.get_name(), str)
+            self.assertIsInstance(tf.get_minutes(), int)
+
+        tf = Timeframe('1D')
+        self.assertEqual(tf.get_minutes(), 1440)
+        self.assertEqual(tf.get_name(), '1-day')
+
+    def test_timeframe_invalid(self):
+        with self.assertRaises(ValueError) as context:
+            tf = Timeframe('1s')
+        # assert context manager
+        self.assertEqual(str(context.exception), "Timeframe period 1s is not supported")
+
+    def test_timeframesToMinutes1(self):
+        self.assertEqual(Timeframe('1m').get_minutes(), 1)
+        self.assertEqual(Timeframe('5m').get_minutes(), 5)
+        self.assertEqual(Timeframe('15m').get_minutes(), 15)
+        self.assertEqual(Timeframe('30m').get_minutes(), 30)
+        self.assertEqual(Timeframe('1h').get_minutes(), 60)
+        self.assertEqual(Timeframe('4h').get_minutes(), 240)
+        self.assertEqual(Timeframe('1D').get_minutes(), 1440)
+        self.assertEqual(Timeframe('1W').get_minutes(), 10080)
+        self.assertEqual(Timeframe('1M').get_minutes(), 43200)
--- Trading/instrument/timeframes.py
+++ Trading/instrument/timeframes.py
@@ -3,3 +3,18 @@ TIMEFRAMES = ['1m', '5m', '15m', '30m', '1h', '4h', '1D', '1W', '1M']
 TIMEFRAME_TO_MINUTES = dict(zip(TIMEFRAMES, [1, 5, 15, 30, 60, 240, 1440, 10080, 43200]))
 TIMEFRAMES_TO_NAME = dict(zip(TIMEFRAMES, ['1-min', '5-min', '15-min', '30-min', '1-hour',
                                             '4-hour', '1-day', '1-week', '1-month']))
+
+class Timeframe:
+    def __init__(self, period: str) -> None:
+        if period not in TIMEFRAMES:
+            raise ValueError(f"Timeframe period {period} is not supported")
+        self.period = period
+
+    def get_minutes(self) -> int:
+        return TIMEFRAME_TO_MINUTES[self.period]
+
+    def get_name(self) -> str:
+        return TIMEFRAMES_TO_NAME[self.period]
+
+    def __str__(self) -> str:
+        return self.period
