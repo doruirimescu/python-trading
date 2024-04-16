87edfaf62c7f494d894ca0b945e92d19f199a023
Doru Irimescu <>
2024-04-16 10:12:26 +0300

    Update constants for yfinance

--- Trading/symbols/constants.py
+++ Trading/symbols/constants.py
@@ -4,16 +4,24 @@ import json
 import os
 from pathlib import Path
 
-from Trading.symbols.xtb.failing_symbols import XTB_FAILING_SYMBOLS
+__CURRENT_FILE_PATH = Path(os.path.dirname(os.path.realpath(__file__)))
 
-CURRENT_FILE_PATH = Path(os.path.dirname(os.path.realpath(__file__)))
+__ALL_SYMBOLS_JSON = Path("all_symbols.json")
+__COMMODITIES_JSON = Path("commodities.json")
+__ETF_JSON = Path("etf.json")
+__FOREX_JSON = Path("forex.json")
+__STOCKS_JSON = Path("stocks.json")
+__INDEX_JSON = Path("index.json")
+
 
 # XTB
-XTB_SYMBOLS_PATH = CURRENT_FILE_PATH.joinpath("xtb/")
-XTB_ALL_SYMBOLS_PATH = XTB_SYMBOLS_PATH.joinpath("all_symbols.json")
-XTB_STOCK_SYMBOLS_PATH = XTB_SYMBOLS_PATH.joinpath("stocks.json")
-XTB_ETF_SYMBOLS_PATH = XTB_SYMBOLS_PATH.joinpath("etf.json")
-XTB_INDEX_SYMBOLS_PATH = XTB_SYMBOLS_PATH.joinpath("index.json")
+__XTB_SYMBOLS_PATH = __CURRENT_FILE_PATH.joinpath("xtb/")
+XTB_ALL_SYMBOLS_PATH = __XTB_SYMBOLS_PATH / __ALL_SYMBOLS_JSON
+XTB_STOCK_SYMBOLS_PATH = __XTB_SYMBOLS_PATH / __STOCKS_JSON
+XTB_FOREX_SYMBOLS_PATH = __XTB_SYMBOLS_PATH / __FOREX_JSON
+XTB_ETF_SYMBOLS_PATH = __XTB_SYMBOLS_PATH / __ETF_JSON
+XTB_INDEX_SYMBOLS_PATH = __XTB_SYMBOLS_PATH / __INDEX_JSON
+XTB_COMMODITY_SYMBOLS_PATH = __XTB_SYMBOLS_PATH / __COMMODITIES_JSON
 
 with open(XTB_ALL_SYMBOLS_PATH, "r") as f:
     XTB_ALL_SYMBOLS_DICT = json.load(f)
@@ -34,9 +42,26 @@ with open(XTB_INDEX_SYMBOLS_PATH, "r") as f:
     XTB_INDEX_SYMBOLS_DICT = json.load(f)
     XTB_INDEX_SYMBOLS = [symbol for symbol in XTB_INDEX_SYMBOLS_DICT]
 
+with open(XTB_COMMODITY_SYMBOLS_PATH, "r") as f:
+    XTB_COMMODITY_SYMBOLS_DICT = json.load(f)
+    XTB_COMMODITY_SYMBOLS = [symbol for symbol in XTB_COMMODITY_SYMBOLS_DICT]
+
+with open(XTB_FOREX_SYMBOLS_PATH, "r") as f:
+    XTB_FOREX_SYMBOLS_DICT = json.load(f)
+    XTB_FOREX_SYMBOLS = [symbol for symbol in XTB_FOREX_SYMBOLS_DICT]
+
 # ALPHASPREAD
-ALPHASPREAD_PATH = CURRENT_FILE_PATH.joinpath("alphaspread/")
+ALPHASPREAD_PATH = __CURRENT_FILE_PATH.joinpath("alphaspread/")
 ALPHASPREAD_URL_PATH = ALPHASPREAD_PATH.joinpath("urls.json")
 
 with open(ALPHASPREAD_URL_PATH, "r") as f:
     ALPHASPREAD_URL_DICT = json.load(f)
+
+# YAHOO FINANCE
+YAHOO_FINANCE_SYMBOLS_PATH = __CURRENT_FILE_PATH.joinpath("yfinance/")
+YAHOO_COMMODITY_SYMBOLS_PATH = YAHOO_FINANCE_SYMBOLS_PATH / __COMMODITIES_JSON
+YAHOO_STOCK_SYMBOLS_PATH = YAHOO_FINANCE_SYMBOLS_PATH / __STOCKS_JSON
+
+with open(YAHOO_STOCK_SYMBOLS_PATH, "r") as f:
+    YAHOO_STOCK_SYMBOLS_DICT = json.load(f)
+    YAHOO_STOCK_SYMBOLS = [symbol for symbol in YAHOO_STOCK_SYMBOLS_DICT]
