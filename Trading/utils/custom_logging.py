8a827768ff9334a03dc0f93cba4ea6b805c97cba
Doru Irimescu <>
2024-05-01 18:15:12 +0300

    Add debugging logger

--- Trading/utils/custom_logging.py
+++ Trading/utils/custom_logging.py
@@ -1,5 +1,13 @@
 import logging
 
+def get_debugging_logger(name: str = 'Debugging logger'):
+    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=FORMAT)
+
+    DEBUGGING_LOGGER = logging.getLogger(name)
+    DEBUGGING_LOGGER.setLevel(logging.DEBUG)
+    DEBUGGING_LOGGER.propagate = True
+    return DEBUGGING_LOGGER
 
 def get_logger(name: str = 'Main logger'):
     FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
