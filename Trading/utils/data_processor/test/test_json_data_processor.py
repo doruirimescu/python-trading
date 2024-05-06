343b6b097d606362ed02c78084b7191514053349
Doru Irimescu <>
2024-05-06 22:31:54 +0300

    Remove test

--- Trading/utils/data_processor/test/test_json_data_processor.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from Trading.utils.data_processor import JsonFileRW
-
-class TestJsonFileProcessor(unittest.TestCase):
-    def test_read_json_file(self):
-        processor = JsonFileRW("test.json")
-        self.assertEqual(processor.data, {})
