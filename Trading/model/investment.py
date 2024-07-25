8943225ca8cc094684bd6c06f0a431b174219271
Doru <doru_metalguitar@yahoo.com>
2024-07-25 17:33:18 +0300

    Refactor and add function to add new precious metal to file

--- Trading/model/investment.py
+++ Trading/model/investment.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 from enum import Enum
 from pydantic import BaseModel
 
+
 class Investment(BaseModel):
     name: str
     symbol: str
@@ -14,16 +15,18 @@ class Investment(BaseModel):
 
 
 class PreciousMetalInvestMentType(str, Enum):
-    coin = "coin",
-    jewelry = "jewelry",
-    tableware = "tableware",
+    coin = ("coin",)
+    jewelry = ("jewelry",)
+    tableware = ("tableware",)
     bullion = "bullion"
 
+
 class PreciousMetal(str, Enum):
-    gold = "gold",
-    silver = "silver",
-    platinum = "platinum",
+    gold = ("gold",)
+    silver = ("silver",)
+    platinum = ("platinum",)
     palladium = "palladium"
+
 
 class PreciousMetalInvestment(Investment):
     type: PreciousMetalInvestMentType
@@ -41,7 +44,9 @@ class Investments:
         cur = self.investments_list[0].entry_price.currency
         for i in self.investments_list:
             if i.entry_price.currency != cur:
-                raise ValueError(f"Ensure all investments are in the same currency. {i.name} is in {i.entry_price.currency}")
+                raise ValueError(
+                    f"Ensure all investments are in the same currency. {i.name} is in {i.entry_price.currency}"
+                )
         return cur
 
     def get_total_invested(self) -> PriceQuote:
@@ -51,6 +56,7 @@ class Investments:
     def summarize(self) -> str:
         total_invested = self.get_total_invested()
         return f"Total invested: {total_invested.price:.2f} {total_invested.currency}\n"
+
 
 class PreciousMetalInvestments:
     def __init__(self, investments: List[PreciousMetalInvestment]) -> None:
@@ -64,14 +70,18 @@ class PreciousMetalInvestments:
         metal = self.investments_list[0].symbol
         for i in self.investments_list:
             if i.symbol != metal:
-                raise ValueError(f"Ensure all investments are in the same metal. {i.name} is in {i.symbol}")
+                raise ValueError(
+                    f"Ensure all investments are in the same metal. {i.name} is in {i.symbol}"
+                )
         return sum([i.volume * i.weight_g * i.purity for i in self.investments_list])
 
     def get_total_impure_weight_g(self):
         metal = self.investments_list[0].symbol
         for i in self.investments_list:
             if i.symbol != metal:
-                raise ValueError(f"Ensure all investments are in the same metal. {i.name} is in {i.symbol}")
+                raise ValueError(
+                    f"Ensure all investments are in the same metal. {i.name} is in {i.symbol}"
+                )
         return sum([i.volume * i.weight_g for i in self.investments_list])
 
     def get_total_purity(self):
@@ -81,37 +91,81 @@ class PreciousMetalInvestments:
         return self.get_total_invested().price / self.get_total_pure_weight_g()
 
     def get_average_market_price_per_gram(self):
-        return sum([i.other["market_price_at_purchase"] for i in self.investments_list]) / len(self.investments_list)
+        return sum(
+            [i.other["market_price_at_purchase"] for i in self.investments_list]
+        ) / len(self.investments_list)
 
     def summarize(self):
         from Trading.utils.chain_exceptions import chain_exceptions
+
         symbol = self.investments_list[0].symbol
         curency = self.investments.get_currency()
 
         exceptions = []
 
-        r = chain_exceptions(lambda: f"Metal: {symbol}\n", exceptions, default_return="")
-        r += chain_exceptions(self.investments.summarize,  exceptions, default_return="")
+        r = chain_exceptions(
+            lambda: f"Metal: {symbol}\n", exceptions, default_return=""
+        )
+        r += chain_exceptions(self.investments.summarize, exceptions, default_return="")
         r += chain_exceptions(
             lambda: f"Total {symbol} weight: {self.get_total_pure_weight_g():.2f} g\n",
-            exceptions, default_return="")
+            exceptions,
+            default_return="",
+        )
         r += chain_exceptions(
             lambda: f"Total weight: {self.get_total_impure_weight_g():.2f} g\n",
-            exceptions, default_return="")
+            exceptions,
+            default_return="",
+        )
         r += chain_exceptions(
             lambda: f"Total (impure) weight: {self.get_total_impure_weight_g():.2f} g\n",
-            exceptions, default_return="")
+            exceptions,
+            default_return="",
+        )
         r += chain_exceptions(
             lambda: f"Total purity: {self.get_total_purity():.2f}\n",
-            exceptions, default_return="")
+            exceptions,
+            default_return="",
+        )
         r += chain_exceptions(
             lambda: f"Average paid price per pure {symbol} gram: {self.get_average_paid_price_per_gram():.3f} {curency}/g\n",
-            exceptions, default_return="")
+            exceptions,
+            default_return="",
+        )
         r += chain_exceptions(
             lambda: f"Average market price per pure {symbol} gram: {self.get_average_market_price_per_gram():.3f} {curency}/g\n",
-            exceptions, default_return="")
+            exceptions,
+            default_return="",
+        )
         if exceptions:
             r += "Exceptions:\n"
             for e in exceptions:
                 r += f"{e}\n"
         return r
+
+
+def add_new_precious_metal_to_file(
+    file_path: str, new_investment: PreciousMetalInvestment
+):
+    from stateful_data_processor.file_rw import JsonFileRW
+
+    file_reader = JsonFileRW(file_path)
+    data = file_reader.read()
+    data.append(new_investment.model_dump())
+    file_reader.write(data)
+
+
+if __name__ == "__main__":
+    path = "/home/.../.../.../Trading/model/silver.json"
+    add_new_precious_metal_to_file(
+        path,
+        PreciousMetalInvestment(
+            name="Silver TEST coin",
+            symbol="silver",
+            entry_price=PriceQuote(price=20, currency="EUR"),
+            volume=100,
+            type=PreciousMetalInvestMentType.coin,
+            weight_g=31.1,
+            purity=0.999,
+        ),
+    )
