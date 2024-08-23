601d3423cb8ff9b55d29304231ea0a6db103ac2d
Doru <doru_metalguitar@yahoo.com>
2024-08-23 21:47:55 +0300

    Extend history

--- Trading/model/history.py
+++ Trading/model/history.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel
 from typing import Optional, List
 from enum import Enum
-
+from datetime import datetime
 class History(BaseModel):
     symbol: Optional[str] = None
     timeframe: Optional[str] = None
-    date: Optional[List[str]] = None
+    date: Optional[List[datetime]] = None
     open: Optional[List[float]] = None
     high: Optional[List[float]] = None
     low: Optional[List[float]] = None
@@ -24,6 +24,46 @@ class History(BaseModel):
             low=self.low[start:end],
             close=self.close[start:end]
         )
+
+    def slice_n_candles_before_date(self, date: str, n: int):
+        # including date
+        index = self.date.index(date)
+        return self.slice(index-n, index+1)
+
+    def sort_by_dates(self):
+        sorted_indices = sorted(range(len(self.date)), key=lambda i: self.date[i])
+        self.date = [self.date[i] for i in sorted_indices]
+        self.open = [self.open[i] for i in sorted_indices]
+        self.high = [self.high[i] for i in sorted_indices]
+        self.low = [self.low[i] for i in sorted_indices]
+        self.close = [self.close[i] for i in sorted_indices]
+
+    def extend(self, history: 'History'):
+        if self.symbol != history.symbol:
+            raise ValueError("Cannot extend history with different symbol")
+        if self.timeframe != history.timeframe:
+            raise ValueError("Cannot extend history with different timeframe")
+
+        self.date.extend(history.date)
+        self.open.extend(history.open)
+        self.high.extend(history.high)
+        self.low.extend(history.low)
+        self.close.extend(history.close)
+
+        date_set = set()
+        date_index = []
+        for i, d in enumerate(self.date):
+            if d in date_set:
+                date_index.append(i)
+            date_set.add(d)
+        for i in reversed(date_index):
+            self.date.pop(i)
+            self.open.pop(i)
+            self.high.pop(i)
+            self.low.pop(i)
+            self.close.pop(i)
+        self.sort_by_dates()
+
     def __getitem__(self, item):
         return getattr(self, item)
 
---
+++ Trading/model/test/test_history.py
@@ -0,0 +1,150 @@
+from unittest import TestCase
+from Trading.model.history import History
+from datetime import datetime
+
+
+class TestHistory(TestCase):
+    def test_init(self):
+        history = History()
+        self.assertIsNone(history.symbol)
+        self.assertIsNone(history.timeframe)
+        self.assertIsNone(history.date)
+        self.assertIsNone(history.open)
+        self.assertIsNone(history.high)
+        self.assertIsNone(history.low)
+        self.assertIsNone(history.close)
+
+    def test_slice(self):
+        history = History(
+            symbol="EURUSD",
+            timeframe="1M",
+            date=[datetime(2021, 1, 1), datetime(2021, 1, 2), datetime(2021, 1, 3)],
+            open=[1.2, 1.3, 1.4],
+            high=[1.3, 1.4, 1.5],
+            low=[1.1, 1.2, 1.3],
+            close=[1.3, 1.4, 1.5],
+        )
+        sliced = history.slice(1, 3)
+        self.assertEqual(sliced.symbol, "EURUSD")
+        self.assertEqual(sliced.timeframe, "1M")
+        self.assertEqual(sliced.date, [datetime(2021, 1, 2), datetime(2021, 1, 3)])
+        self.assertEqual(sliced.open, [1.3, 1.4])
+        self.assertEqual(sliced.high, [1.4, 1.5])
+        self.assertEqual(sliced.low, [1.2, 1.3])
+        self.assertEqual(sliced.close, [1.4, 1.5])
+
+        history = History(
+            symbol="EURUSD",
+            timeframe="1M",
+            date=[
+                datetime(2021, 1, 1),
+                datetime(2021, 1, 2),
+                datetime(2021, 1, 3),
+                datetime(2021, 1, 4),
+            ],
+            open=[1.2, 1.3, 1.4, 1.5],
+            high=[1.3, 1.4, 1.5, 1.5],
+            low=[1.1, 1.2, 1.3, 1.3],
+            close=[1.3, 1.4, 1.5, 1.5],
+        )
+        sliced = history.slice(-2)
+        self.assertEqual(sliced.symbol, "EURUSD")
+        self.assertEqual(sliced.timeframe, "1M")
+        self.assertEqual(sliced.date, [datetime(2021, 1, 3), datetime(2021, 1, 4)])
+        self.assertEqual(sliced.open, [1.4, 1.5])
+        self.assertEqual(sliced.high, [1.5, 1.5])
+        self.assertEqual(sliced.low, [1.3, 1.3])
+        self.assertEqual(sliced.close, [1.5, 1.5])
+
+    def test_slice_n_candles_before_date(self):
+        history = History(
+            symbol="EURUSD",
+            timeframe="1M",
+            date=[
+                datetime(2021, 1, 1),
+                datetime(2021, 1, 2),
+                datetime(2021, 1, 3),
+                datetime(2021, 1, 4),
+            ],
+            open=[1.2, 1.3, 1.4, 1.5],
+            high=[1.3, 1.4, 1.5, 1.5],
+            low=[1.1, 1.2, 1.3, 1.3],
+            close=[1.3, 1.4, 1.5, 1.5],
+        )
+        sliced = history.slice_n_candles_before_date(datetime(2021, 1, 3), 2)
+        print(sliced)
+        self.assertEqual(sliced.symbol, "EURUSD")
+        self.assertEqual(sliced.timeframe, "1M")
+        self.assertEqual(sliced.date, [datetime(2021, 1, 1), datetime(2021, 1, 2), datetime(2021, 1, 3)])
+        self.assertEqual(sliced.open, [1.2, 1.3, 1.4])
+        self.assertEqual(sliced.high, [1.3, 1.4, 1.5])
+        self.assertEqual(sliced.low, [1.1, 1.2, 1.3])
+        self.assertEqual(sliced.close, [1.3, 1.4, 1.5])
+
+    def test_extend(self):
+        history = History(
+            symbol="EURUSD",
+            timeframe="1M",
+            date=[datetime(2021, 1, 1), datetime(2021, 1, 2), datetime(2021, 1, 3)],
+            open=[1.2, 1.3, 1.4],
+            high=[1.3, 1.4, 1.5],
+            low=[1.1, 1.2, 1.3],
+            close=[1.3, 1.4, 1.5],
+        )
+        history2 = History(
+            symbol="EURUSD",
+            timeframe="1M",
+            date=[datetime(2021, 1, 4), datetime(2021, 1, 5), datetime(2021, 1, 6)],
+            open=[1.5, 1.6, 1.7],
+            high=[1.6, 1.7, 1.8],
+            low=[1.4, 1.5, 1.6],
+            close=[1.6, 1.7, 1.8],
+        )
+        history.extend(history2)
+        self.assertEqual(history.symbol, "EURUSD")
+        self.assertEqual(history.timeframe, "1M")
+        self.assertEqual(
+            history.date,
+            [
+                datetime(2021, 1, 1),
+                datetime(2021, 1, 2),
+                datetime(2021, 1, 3),
+                datetime(2021, 1, 4),
+                datetime(2021, 1, 5),
+                datetime(2021, 1, 6),
+            ],
+        )
+        self.assertEqual(history.open, [1.2, 1.3, 1.4, 1.5, 1.6, 1.7])
+        self.assertEqual(history.high, [1.3, 1.4, 1.5, 1.6, 1.7, 1.8])
+        self.assertEqual(history.low, [1.1, 1.2, 1.3, 1.4, 1.5, 1.6])
+        self.assertEqual(history.close, [1.3, 1.4, 1.5, 1.6, 1.7, 1.8])
+
+        # test that duplicates are removed
+        history = History(
+            symbol="EURUSD",
+            timeframe="1M",
+            date=[datetime(2021, 1, 1), datetime(2021, 1, 2), datetime(2021, 1, 3), datetime(2022, 1, 1)],
+            open= [1.2, 1.3, 1.4, 1.5],
+            high= [1.3, 1.4, 1.5, 1.6],
+            low=  [1.1, 1.2, 1.01, 1.4],
+            close=[1.3, 1.4, 1.9, 1.6],
+        )
+        history.extend(history2)
+        self.assertEqual(history.symbol, "EURUSD")
+        self.assertEqual(history.timeframe, "1M")
+        self.assertEqual(
+            history.date,
+            [
+                datetime(2021, 1, 1),
+                datetime(2021, 1, 2),
+                datetime(2021, 1, 3),
+                datetime(2021, 1, 4),
+                datetime(2021, 1, 5),
+                datetime(2021, 1, 6),
+                datetime(2022, 1, 1),
+            ],
+        )
+        self.assertEqual(history.open, [1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.5])
+        self.assertEqual(history.high, [1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.6])
+        self.assertEqual(history.low, [1.1, 1.2, 1.01, 1.4, 1.5, 1.6, 1.4])
+        self.assertEqual(history.close, [1.3, 1.4, 1.9, 1.6, 1.7, 1.8, 1.6])
