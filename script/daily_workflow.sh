a579fccc1c23152df35334c693e0ba40f67b0977
Doru <doru_metalguitar@yahoo.com>
2026-05-13 22:36:50 +0300

    Fix alert, publish latest analysis to index

--- script/daily_workflow.sh
+++ script/daily_workflow.sh
@@ -5,3 +5,5 @@ python3 Trading/stock/analyze_nasdaq.py --nasdaq
 
 pip install -q "kaleido==0.2.1"
 python3 Trading/stock/visualize.py --nasdaq --save
+
+cp "Trading/generated/nasdaq/nasdaq_analysis_$(date +%Y-%m-%d).html" docs/index.html
